41587f9d80df52599171ab5e528481374010229d
Adrian Gruber <gruber950513@gmail.com>
2019-12-03 16:58:22 +0100

    Add animation to the CreateTodoButton

--- src/components/CreateTodoButton/CreateTodoButton.tsx
+++ src/components/CreateTodoButton/CreateTodoButton.tsx
@@ -1,17 +1,29 @@
 import React from 'react';
 
+import { useHistory, useLocation } from 'react-router';
+
 import classes from './CreateTodoButton.module.scss';
-import { useHistory } from 'react-router';
+
+const CREATE_TODO_PAGE = '/todo/create';
 
 function CreateTodoButton() {
   const history = useHistory();
+  const location = useLocation();
 
   const navigateToCreateTodoPage = () => {
-    history.push('/todo/create');
+    history.push(CREATE_TODO_PAGE);
+  };
+
+  const isCreateTodoPage = () => {
+    return location.pathname && !location.pathname.includes(CREATE_TODO_PAGE);
   };
 
   return (
-    <button className={classes.CreateTodoButton} onClick={navigateToCreateTodoPage}>
+    <button
+      className={classes.CreateTodoButton}
+      style={{ display: isCreateTodoPage() ? 'block' : 'none' }}
+      onClick={navigateToCreateTodoPage}
+    >
       <span role="img" aria-label="create-todo">
         ➕
       </span>
