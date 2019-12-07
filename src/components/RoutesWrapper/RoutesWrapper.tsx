a65f359c133c8b53cba285b260e936da7561998f
Adrian Gruber <gruber950513@gmail.com>
2019-12-07 10:14:03 +0100

    Add animation to page transitions

--- src/components/RoutesWrapper/RoutesWrapper.tsx
+++ src/components/RoutesWrapper/RoutesWrapper.tsx
@@ -1,6 +1,7 @@
 import React, { Suspense } from 'react';
 
-import { Switch, Route, Redirect } from 'react-router-dom';
+import { Switch, Route, Redirect, useLocation } from 'react-router-dom';
+import { useTransition, animated } from 'react-spring';
 
 import { LoadingIndicator } from '../LoadingIndicator';
 
@@ -10,28 +11,40 @@ const CreateTodoPage = React.lazy(() => import('../../pages/CreateTodoPage/Creat
 const PageNotFound = React.lazy(() => import('../../pages/PageNotFound/PageNotFound'));
 
 function RoutesWrapper() {
+  const location = useLocation();
+  const transitions = useTransition(location, location => location.pathname, {
+    initial: null,
+    from: { opacity: 0, transform: 'translate3d(100%,0,0)' },
+    enter: { opacity: 1, transform: 'translate3d(0%,0,0)' },
+    leave: { opacity: 0, transform: 'translate3d(-50%,0,0)', position: 'absolute', zIndex: -1 },
+  });
+
   return (
-    <div>
-      <Suspense fallback={<LoadingIndicator />}>
-        <Switch>
-          <Route path="/" exact>
-            <Redirect to="/todos" />
-          </Route>
-          <Route path="/board">
-            <BoardPage />
-          </Route>
-          <Route path="/todos">
-            <TodosPage />
-          </Route>
-          <Route path="/todo/create">
-            <CreateTodoPage />
-          </Route>
-          <Route path="*">
-            <PageNotFound />
-          </Route>
-        </Switch>
-      </Suspense>
-    </div>
+    <>
+      {transitions.map(({ item: location, props, key }) => (
+        <animated.div key={key} style={props}>
+          <Suspense fallback={<LoadingIndicator />}>
+            <Switch location={location}>
+              <Route path="/" exact>
+                <Redirect to="/todos" />
+              </Route>
+              <Route path="/board">
+                <BoardPage />
+              </Route>
+              <Route path="/todos">
+                <TodosPage />
+              </Route>
+              <Route path="/todo/create">
+                <CreateTodoPage />
+              </Route>
+              <Route path="*">
+                <PageNotFound />
+              </Route>
+            </Switch>
+          </Suspense>
+        </animated.div>
+      ))}
+    </>
   );
 }
 
