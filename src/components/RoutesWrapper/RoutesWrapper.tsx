7eeea04308a8c0be1d5629b195ad1d17844601a1
Adrian Gruber <gruber950513@gmail.com>
2019-12-09 09:05:22 +0100

    Fix narrowing screen at navigation's leave stage

--- src/components/RoutesWrapper/RoutesWrapper.tsx
+++ src/components/RoutesWrapper/RoutesWrapper.tsx
@@ -16,7 +16,7 @@ function RoutesWrapper() {
     initial: null,
     from: { opacity: 0, transform: 'translate3d(100%,0,0)' },
     enter: { opacity: 1, transform: 'translate3d(0%,0,0)' },
-    leave: { opacity: 0, transform: 'translate3d(-50%,0,0)', position: 'absolute', zIndex: -1 },
+    leave: { opacity: 0, transform: 'translate3d(-50%,0,0)', width: '100%', position: 'absolute', zIndex: -1 },
   });
 
   return (
