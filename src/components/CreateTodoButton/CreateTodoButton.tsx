3e05fb0c661892e154d3f74ee98cb48dedc6bb8c
Adrian Gruber <gruber950513@gmail.com>
2019-12-09 14:55:41 +0100

    Animate + button with react-spring

--- src/components/CreateTodoButton/CreateTodoButton.tsx
+++ src/components/CreateTodoButton/CreateTodoButton.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import { useHistory, useLocation } from 'react-router';
+import { animated, useSpring } from 'react-spring';
 
 import classes from './CreateTodoButton.module.scss';
 
@@ -15,19 +16,24 @@ function CreateTodoButton() {
   };
 
   const isCreateTodoPage = () => {
-    return location.pathname && !location.pathname.includes(CREATE_TODO_PAGE);
+    return !!(location.pathname && !location.pathname.includes(CREATE_TODO_PAGE));
   };
 
+  const animation = useSpring({
+    to: { opacity: 1 },
+    from: { opacity: 0 },
+    reverse: !isCreateTodoPage(),
+    reset: !isCreateTodoPage(),
+  });
+
   return (
-    <button
-      className={classes.CreateTodoButton}
-      style={{ display: isCreateTodoPage() ? 'block' : 'none' }}
-      onClick={navigateToCreateTodoPage}
-    >
-      <span role="img" aria-label="create-todo">
-        ➕
-      </span>
-    </button>
+    <animated.div style={animation}>
+      <button className={classes.CreateTodoButton} onClick={navigateToCreateTodoPage}>
+        <span role="img" aria-label="create-todo">
+          ➕
+        </span>
+      </button>
+    </animated.div>
   );
 }
 
